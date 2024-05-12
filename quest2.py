661e2ac51872900ab1aa6957eb5eea9b608cefd1
Md Taqui Ansari <142133106+taquiansari@users.noreply.github.com>
2024-05-12 19:43:55 +0530

    Update quest2.py

--- quest2.py
+++ quest2.py
@@ -1,9 +1,17 @@
-arr = [int(x) for x in input("Enter elements of the array separated by space: ").split()]
-l,r,sum=0,1,0
-while(r<len(arr)):
-    if arr[l]>arr[r]:
-        sum+=arr[l]-arr[r]
-    else:
-        l=r
-    r+=1
-print(sum)
+from typing import List
+height = [int(x) for x in input("Enter elements of the array separated by space: ").split()]
+# min(L,R)-h[i]
+def trappedwater(height: List[int]) ->int:
+    l,r,sum=0,len(height)-1,0
+    lmax,rmax =height[l],height[r]
+    while(l<r):
+        if lmax<rmax:
+            l+=1
+            lmax = max(lmax,height[l])
+            sum += lmax - height[l]
+        else:
+            r-=1
+            rmax = max(rmax,height[r])
+            sum += rmax - height[r]
+    return sum
+print("Trapped water:", trappedwater(height))
