d5286be047d1f87c9691f5f3743ecaa24c4d72b6
Md Taqui Ansari <142133106+taquiansari@users.noreply.github.com>
2024-05-12 18:46:14 +0530

    Add files via upload

---
+++ quest2.py
@@ -0,0 +1,7 @@
+arr = [int(x) for x in input("Enter elements of the array separated by space: ").split()]
+l,r,sum=0,1,0
+while(r<len(arr)):
+    if arr[l]>arr[r]:
+        sum+=arr[l]-arr[r]
+    r+=1
+print(sum)
